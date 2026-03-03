5d62482025a9f04270bacc2bc4bf72f050e12c8f
terrin <727824tuio040@skct.edu.in>
2026-03-03 10:32:46 +0530

    Removed merge conflict markers

--- main_service.py
+++ main_service.py
@@ -30,8 +30,4 @@ def machine_data():
         "health": health,
         "risk": risk,
         "recommendation": recommendation
-<<<<<<< HEAD
     }
-=======
-    }
->>>>>>> 0b362ad3bdd5b14df3289c0b9b29489202ca3d0e
